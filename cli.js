7af217a1ee7a6de8e3817a6b66c8026090264fe8
Nick Aliferov <x8core@gmail.com>
2026-03-13 23:27:55 +0200

    add command fow show procs, update index.html

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 import { createSupervisor } from './src/supervisor/supervisor.js'
+import { getProcessList } from './src/supervisor/utils.js'
 
 const [command, ...args] = process.argv.slice(2)
 
@@ -6,7 +7,16 @@ const commands = {
   'start-supervisor': async () => {
       const supervisor = createSupervisor()
       await supervisor.start()
-    },
+  },
+  'list-procs': async () => {
+    const processList = await getProcessList()
+    for (const pid in processList) {
+      const command = processList[pid].command
+      if (command.includes('service-manager/src/services')) {
+        console.log(command)
+      }
+    }
+  },
 }
 
 const exec = commands[command]
